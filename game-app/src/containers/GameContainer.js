c8127ea6b773fd3965a0a0df15504bc9e6288490
Cleyra Uzcategui <cleyra.uzcategui@gmail.com>
2018-09-27 14:03:43 +0100

    Created answer component

--- game-app/src/containers/GameContainer.js
+++ game-app/src/containers/GameContainer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Stars from '../components/Stars';
+import Button from '../components/Button';
+import Answer from '../components/Answer';
 
 class GameContainer extends Component {
   render()
@@ -8,6 +10,8 @@ class GameContainer extends Component {
      <div>
        <h3>Play Nine</h3>
        <Stars />
+       <Button />
+       <Answer />
      </div>
    );
  };
